33c8e19b9699e2454fe45b641fe03fa88f361157
unknown <rajajamalparvez@gmail.com>
2019-04-12 20:14:13 +0500

    Added Client.Test and Added DbContext

---
+++ Ecom.Data/Contexts/EcomDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Ecom.Domain.Entities.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecom.Data.Contexts
+{
+    public class EcomDbContext : DbContext
+    {
+        public EcomDbContext(DbContextOptions<EcomDbContext> builder) : 
+            base(builder) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+           var connection = @"Data Source=(LOCALDB)\MSSQLLocalDB;Initial Catalog=ecommercedb;Integrated Security=True;Pooling=False";
+           builder.UseSqlServer(connection);
+        }
+    }
+}
